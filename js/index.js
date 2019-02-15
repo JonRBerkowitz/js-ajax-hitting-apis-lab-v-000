04c5d80ab68dfa1839f4e13827c74eae8db23dcd
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 22:28:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ function getRepositories() {
 
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
-  const repoList = `${repos.map(r => '<li>' + r.name + '- <a href="#" data-repo=""' + r.name  '" onclick="getCommits(this)">Get Commits</li>').join('')}`;
+  const repoList = `${repos.map(r => '<li>' + r.name + '- <a href="#" data-repo="' + r.name  '" onclick="getCommits(this)">Get Commits</a></li>').join('')}`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getCommits() {
-  
+
 }
