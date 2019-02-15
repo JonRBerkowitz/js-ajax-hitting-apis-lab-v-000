9dd84e0744e3df85533ce6bc08e582608446c99d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 22:57:41 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,10 +9,22 @@ function getRepositories() {
 
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
-  const repoList = `${repos.map(r => '<li>' +
-  r.name + '- <a href="#" data-repo="' +
-  r.name + '" data-user="' + r.owner.login +
-  '" onclick="getCommits(this)">Get Commits</a></li>').join('')}`;
+  const repoList =
+      '<ul>' +
+      repos
+        .map(repo => {
+          const dataUsername = 'data-username="' + repo.owner.login + '"';
+          const dataRepoName = 'data-repository="' + repo.name + '"';
+          return `
+            <li>
+              <h2>${repo.name}</h2>
+              <a href="${repo.html_url}">${repo.html_url}</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+            </li>`;
+        })
+        .join('') +
+      '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
 
