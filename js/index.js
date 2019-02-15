3931647b006e596f1789bfbc9b3d16ece046635e
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 22:41:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,14 +11,32 @@ function showRepositories() {
   var repos = JSON.parse(this.responseText);
   const repoList = `${repos.map(r => '<li>' +
   r.name + '- <a href="#" data-repo="' +
-  r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}`;
+  r.name + '" data-user="' + r.owner.login +
+  '" onclick="getCommits(this)">Get Commits</a></li>').join('')}`;
   document.getElementById('repositories').innerHTML = repoList;
+  console.log(r.owner.login);
 }
 
 function getCommits(e) {
   const name = e.dataset.repo;
+  const user = e.dataset.user;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
-  req.open('GET', "https://api.github.com/users/" + user + "/repos");
+  req.open('GET', "https://api.github.com/repos/" + user + "/" + name + "/commits");
   req.send();
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
 }
