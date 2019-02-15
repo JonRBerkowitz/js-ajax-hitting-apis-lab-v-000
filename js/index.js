1d6528909800ff3565238c9ddcc467f87f4e0336
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 22:03:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 function getRepositories() {
   const req = new XMLHttpRequest();
   let user = document.getElementById("username").value;
+  req.addEventListener('load', showRepositories);
   req.open('GET', "https://api.github.com/users/" + user + "/repos");
   req.send;
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
 }
