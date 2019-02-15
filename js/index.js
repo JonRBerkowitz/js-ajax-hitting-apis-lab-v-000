09a2fc32a9d6bdb0a182143f28d4502b95b63988
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 22:55:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -43,9 +43,9 @@ function displayCommits() {
 }
 
 function getBranches(el) {
-  const repoName = el.dataset.repository;
+  const repo = el.dataset.repo;
   const uri =
-    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/branches';
+    rootURL + '/repos/' + el.dataset.user + '/' + repo + '/branches';
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', displayBranches);
   xhr.open('GET', uri);
